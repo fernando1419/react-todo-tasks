d727d8e465798ffe730fc424b08229c88f42ec86
Fernando Lamas <lamas.fernando1419@gmail.com>
2025-04-21 18:20:06 -0300

    Add Spinner component

---
+++ src/components/shared/Spinner.tsx
@@ -0,0 +1,9 @@
+import "./Spinner.css"; // We'll add the CSS next
+
+export default function Spinner() {
+  return (
+    <div className="spinner-wrapper">
+      <div className="spinner"></div>
+    </div>
+  );
+}
